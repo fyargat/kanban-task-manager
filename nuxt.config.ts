754b422892cb20d186e4158d83e8a813bd18e59c
Cass <bikbaevf18@gmail.com>
2023-10-06 20:06:01 +0400

    Update nuxt.config.ts
    Disable storybook module
    Add options for build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: true },
-	modules: ["@nuxtjs/stylelint-module", "@nuxtjs/storybook", "@pinia/nuxt"],
-	css: ["~/assets/scss/index.scss"],
+	// modules: ["@nuxtjs/stylelint-module", "@nuxtjs/storybook", "@pinia/nuxt"],
+	modules: ["@nuxtjs/stylelint-module", "@pinia/nuxt"],
+
+	css: ["@/assets/scss/index.scss"],
 	components: {
 		dirs: [],
 	},
 	pages: true,
+	app: {
+		buildAssetsDir: "assets",
+	},
+	experimental: {
+		payloadExtraction: false,
+	},
+	vite: {
+		server: {
+			hmr: {
+				protocol: "ws",
+				host: "localhost",
+			},
+		},
+	},
+	ssr: false,
 });
