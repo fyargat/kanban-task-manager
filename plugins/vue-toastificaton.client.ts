956f7e9a1a057680b356c11054069fcb01e0d9f8
Cass <bikbaevf18@gmail.com>
2023-10-07 16:40:36 +0400

    Configure toast

--- plugins/vue-toastificaton.client.ts
+++ plugins/vue-toastificaton.client.ts
@@ -1,5 +1,12 @@
-import Toast from "vue-toastification";
+import Toast, { PluginOptions } from "vue-toastification";
+
+import "~/assets/scss/_toast.scss";
+
+const options: PluginOptions = {
+	timeout: 3000,
+	closeButton: false,
+};
 
 export default defineNuxtPlugin((nuxtApp) => {
-	nuxtApp.vueApp.use(Toast);
+	nuxtApp.vueApp.use(Toast, options);
 });
